fa717ecc8df50ced47ba9c7a5285f09c8c0b1df6
Th1Thanakorn <80148630+Th1Thanakorn@users.noreply.github.com>
2023-12-28 21:22:38 +0700

    gameplay 2

--- Matrix.cs
+++ Matrix.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
 
 public class Matrix : MonoBehaviour
 {
